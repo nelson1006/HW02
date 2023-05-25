9944dd7543b65cec14cc642b8a4db4fe77fc97ad
nelson1006 <125860809+nelson1006@users.noreply.github.com>
2023-05-25 12:53:37 +0800

    HW02

--- app/src/main/java/com/example/gamevertest/PlayerAdapter.kt
+++ app/src/main/java/com/example/gamevertest/PlayerAdapter.kt
@@ -6,7 +6,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gamevertest.PlayerModel
 
-class PlayerAdapter(private val playerlist:ArrayList<PlayerModel>): RecyclerView.Adapter<PlayerAdapter.ViewHolder>() {
+class PlayerAdapter(private val playerList:ArrayList<PlayerModel>): RecyclerView.Adapter<PlayerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerAdapter.ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.player_list_item,parent,false)
@@ -14,13 +14,13 @@ class PlayerAdapter(private val playerlist:ArrayList<PlayerModel>): RecyclerView
     }
 
     override fun onBindViewHolder(holder: PlayerAdapter.ViewHolder, position: Int) {
-        val currentPlayer = playerlist[position]
+        val currentPlayer = playerList[position]
         holder.showPlayerName.text = currentPlayer.playerName
     }
 
 
     override fun getItemCount(): Int {
-        return playerlist.size
+        return playerList.size
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
